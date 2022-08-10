70acd306b6ec81fd38e1ff864d65e9f2d9e38a9b
Vlad <vlad.gheorghita.s@outlook.com>
2022-08-10 02:45:56 -0700

    log out button added

--- src/app/core/nav/nav.component.ts
+++ src/app/core/nav/nav.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { faBuildingCircleArrowRight, faHouseUser } from '@fortawesome/free-solid-svg-icons';
+import { IconProp } from '@fortawesome/fontawesome-svg-core';
+import { faArrowRightFromBracket, faBuildingCircleArrowRight, faHouseUser, IconDefinition } from '@fortawesome/free-solid-svg-icons';
+import { AuthService } from 'src/app/libs/auth/auth.service';
 
 @Component({
   selector: 'app-nav',
@@ -7,15 +9,20 @@ import { faBuildingCircleArrowRight, faHouseUser } from '@fortawesome/free-solid
   styleUrls: ['./nav.component.scss']
 })
 export class NavComponent implements OnInit {
+  iconList: Map<string, any> = new Map();
 
-  faOffice: any;
-  faHome: any;
-
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit(): void {
-    this.faOffice = faBuildingCircleArrowRight;
-    this.faHome = faHouseUser
+    this.iconList.set("logout", faArrowRightFromBracket);
+    this.iconList.set("office", faBuildingCircleArrowRight);
+    this.iconList.set("home", faHouseUser);
+
+    var tst = this.iconList.get("office")
+  }
+
+  logOut(){
+    this.authService.SignOut();
   }
 
 }
