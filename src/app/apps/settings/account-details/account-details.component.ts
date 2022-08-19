a3e6a932ae84ac4d528c6e5dca8a50aaf270f6c6
Vlad <vlad.gheorghita.s@outlook.com>
2022-08-19 14:56:45 +0300

    configured account details

--- src/app/apps/settings/account-details/account-details.component.ts
+++ src/app/apps/settings/account-details/account-details.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { faCircleUser, faPen } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-account-details',
@@ -6,10 +8,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./account-details.component.scss']
 })
 export class AccountDetailsComponent implements OnInit {
+  unknownUserIcon = faCircleUser;
+  editIcon = faPen;
 
+  isEditUsername = false;
+  isEditEmail = false;
+
+  usernameForm: FormControl = new FormControl('',[Validators.required]);
+  emailForm: FormControl = new FormControl('', [Validators.required, Validators.email]);
+  
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  editUsername(){
+    this.isEditUsername = true;
+  }
+
+  editEmail(){
+    this.isEditEmail = true;
+  }
+
+  onChangeUsername(){
+    this.isEditUsername = false;
+    this.usernameForm.setValue('');
+  }
+
+  onChangeEmail(){
+    this.isEditEmail = false;
+    this.emailForm.setValue('');
+  }
 }
--- src/app/apps/settings/settings.module.ts
+++ src/app/apps/settings/settings.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { SettingsRoutingModule } from './settings-routing.module';
 import { SettingsComponent } from './settings.component';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 //Font Awesome
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 //Flex-Layout
-import {  FlexLayoutModule } from '@angular/flex-layout';
+import { FlexLayoutModule } from '@angular/flex-layout';
 //Material Buttons & Indicators
 import { MatButtonModule } from '@angular/material/button';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
@@ -14,6 +16,10 @@ import { AccountDetailsComponent } from './account-details/account-details.compo
 import { ChangePasswordComponent } from './change-password/change-password.component';
 //Material Layout
 import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
+//Material Form Controls
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 
 @NgModule({
@@ -30,6 +36,11 @@ import { MatCardModule } from '@angular/material/card';
     MatButtonToggleModule,
     MatButtonModule,
     MatCardModule,
+    MatGridListModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    FormsModule
   ]
 })
 export class SettingsModule { }
