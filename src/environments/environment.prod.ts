419149fa897ca3879f67198f54ae8779200cf7e6
Vlad <vlad.gheorghita.s@outlook.com>
2022-08-20 20:22:06 +0300

    refactoring

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDFQOeVKPY1QfL_bmyBqGIzpNzzNuMAZqo",
+    authDomain: "wfh-calendar-be679.firebaseapp.com",
+    projectId: "wfh-calendar-be679",
+    storageBucket: "wfh-calendar-be679.appspot.com",
+    messagingSenderId: "881271965924",
+    appId: "1:881271965924:web:71017f5bb9a9acb73e04f1",
+    measurementId: "G-K7X1QTXXY3"
+  },
 };
